8e2b25dd26bdc0196d2ef8e62e8863c71727df91
Proskurova <proskurovalyuda@gmail.com>
2023-01-04 14:34:13 +0300

    changed the settings

--- Lyuda_2022/Lyuda_2022/settings.py
+++ Lyuda_2022/Lyuda_2022/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s9wuy=vum(6rj)c(!w&s5e!8-innfhw!op2w4_41i33r9^axfp'
+SECRET_KEY = secret.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
